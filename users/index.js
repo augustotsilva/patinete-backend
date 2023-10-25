cfdeaeac20cd015d27caf6c8dcf9aa46dc26da95
augustotsilva <atexeira13@gmail.com>
2023-10-25 19:53:12 -0300

    PORT

--- users/index.js
+++ users/index.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import bodyParser from "body-parser";
 
 const app = express();
+const PORT = 3000;
+
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -10,4 +12,4 @@ app.get("/user", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(3000);
+app.listen(PORT);
