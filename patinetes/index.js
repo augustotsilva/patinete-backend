283001a4c5eb37233b628b01c9af24ee6a4662d5
Mateus Pereira <mateus.sle.per@hotmail.com>
2023-10-27 16:16:25 -0300

    CRUD do patinete

--- patinetes/index.js
+++ patinetes/index.js
@@ -16,10 +16,19 @@ app.get("/patinete", async (req, res) => {
   return res.send(patinetes);
 });
 
+app.get("/patinete/:id", async (req, res) => {
+  const patinete = await prisma.patinete.findUnique({
+    where: { id: req.params.id },
+  });
+  if (!patinete) return res.status(404).send("Patinete not found!");
+  return res.send(patinete);
+});
+
 const schema = Joi.object({
   status: Joi.string().required().valid("DISPONIVEL", "ALUGADO", "INATIVO"),
   latitude: Joi.number().required(),
   longitude: Joi.number().required(),
+  rentAt: Joi.date().iso().optional().allow(null),
 });
 
 app.post("/patinete", async (req, res) => {
@@ -27,14 +36,68 @@ app.post("/patinete", async (req, res) => {
     status: req.body.status,
     latitude: req.body.latitude,
     longitude: req.body.longitude,
+    rentAt: req.body.rentAt,
   };
   const { error } = schema.validate(patineteData);
   if (error) return res.status(400).send(error.details[0].message);
+
+  if (!isNaN(new Date(patineteData.rentAt))) {
+    const rentAt = new Date(patineteData.rentAt);
+    const patinete = await prisma.patinete.create({
+      data: { ...patineteData, rentAt },
+    });
+    return res.status(201).send(patinete);
+  }
 
   const patinete = await prisma.patinete.create({
     data: patineteData,
   });
   return res.status(201).send(patinete);
+});
+
+app.put("/patinete/:id", async (req, res) => {
+  const patinete = await prisma.patinete.findUnique({
+    where: { id: req.params.id },
+  });
+  if (!patinete) return res.status(404).send("Patinete not found!");
+
+  const patineteData = {
+    status: req.body.status,
+    latitude: req.body.latitude,
+    longitude: req.body.longitude,
+    rentAt: req.body.rentAt,
+  };
+  const { error } = schema.validate(patineteData);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  if (!isNaN(new Date(patineteData.rentAt))) {
+    const rentAt = new Date(patineteData.rentAt);
+    const updated = await prisma.patinete.update({
+      where: { id: patinete.id },
+      data: {
+        ...patineteData,
+        rentAt,
+      },
+    });
+    return res.send(updated);
+  }
+  const updated = await prisma.patinete.update({
+    where: { id: patinete.id },
+    data: patineteData,
+  });
+  return res.status(201).send(updated);
+});
+
+app.delete("/patinete/:id", async (req, res) => {
+  const patinete = await prisma.patinete.findUnique({
+    where: { id: req.params.id },
+  });
+  if (!patinete) return res.status(404).send("Patinete not found!");
+
+  await prisma.patinete.delete({
+    where: { id: req.params.id },
+  });
+  return res.status(204).send([]);
 });
 
 app.listen(process.env.PORT || 3001);
